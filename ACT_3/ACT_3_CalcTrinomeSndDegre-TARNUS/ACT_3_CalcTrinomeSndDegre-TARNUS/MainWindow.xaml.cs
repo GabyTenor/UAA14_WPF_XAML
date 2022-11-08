6a3ce76e094a6dbceed3a807cd3be8272658b9cc
Tarnus Gabriel <200198@CCM-Asty-Moulin.local>
2022-11-08 14:58:18 +0100

    Calculette

--- ACT_3/ACT_3_CalcTrinomeSndDegre-TARNUS/ACT_3_CalcTrinomeSndDegre-TARNUS/MainWindow.xaml.cs
+++ ACT_3/ACT_3_CalcTrinomeSndDegre-TARNUS/ACT_3_CalcTrinomeSndDegre-TARNUS/MainWindow.xaml.cs
@@ -23,6 +23,82 @@ namespace ACT_3_CalcTrinomeSndDegre_TARNUS
         public MainWindow()
         {
             InitializeComponent();
+            a.PreviewTextInput += new TextCompositionEventHandler(VerifTextInput);
+            b.PreviewTextInput += new TextCompositionEventHandler(VerifTextInput);
+            c.PreviewTextInput += new TextCompositionEventHandler(VerifTextInput);
+
+            calculate.MouseEnter += new MouseEventHandler(SurvolButton);
+            calculate.MouseLeave += new MouseEventHandler(SurvolButtonFin);
+            calculate.Click += new RoutedEventHandler(Calculer)
+;
+           
+        }
+        public void VerifTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if(e.Text !="," && e.Text !="-" && !EstEntier(e.Text))
+            {
+                e.Handled = true;
+            }
+            else if(e.Text == ",")
+            {
+                if (((TextBox)sender).Text.IndexOf(e.Text) > -1)
+                {
+                    e.Handled = true;
+                }
+            }else if(e.Text == "-")
+            {
+                if (((TextBox)sender).Text.IndexOf(e.Text) > -1)
+                {
+                    e.Handled = true;
+                }
+            }
+            
+        }
+
+        public bool EstEntier(string text)
+        {
+            bool isInt;
+            
+            int entier;
+
+            if (int.TryParse(text, out entier))
+            {
+                isInt = true;
+            }
+            else
+            {
+                isInt = false;
+            }         
+                      
+            return isInt;
+        }
+
+        public void SurvolButton(object sender, MouseEventArgs e)
+        {
+            hiddenButton.Visibility = Visibility.Visible;
+            hiddenButton.Background = Brushes.Red;           
+        }
+
+        public void SurvolButtonFin(object sender, MouseEventArgs e)
+        {
+            hiddenButton.Visibility = Visibility.Hidden;
+            hiddenButton.Background = Brushes.Gray;      
+        }
+
+        public void Calculer(object sender, RoutedEventArgs e)
+        {
+            if(double.TryParse(a.Text, out double aValue) && double.TryParse(b.Text, out double bValue) && double.TryParse(c.Text, out double cValue))
+            {
+                MethodesDuProjet mesOutils = new MethodesDuProjet();
+
+                string message;
+                mesOutils.Resolution(aValue, bValue, cValue, out message);
+
+                Resultat affichage = new Resultat();
+                affichage.reponse.Text = message;
+                affichage.Show();
+            }
+          
         }
     }
 }
---
+++ ACT_3/ACT_3_CalcTrinomeSndDegre-TARNUS/ACT_3_CalcTrinomeSndDegre-TARNUS/MethodesDuProjet.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ACT_3_CalcTrinomeSndDegre_TARNUS
+{
+    class MethodesDuProjet
+    {
+        public void Resolution(double a, double b, double c, out string message)
+        {
+            double delta = Math.Pow(b, 2) - 4 * a * c;
+
+            if(delta > 0)
+            {
+                double x1 = (-b + Math.Sqrt(delta)) / 2 * a;
+                
+                double x2 = (-b - Math.Sqrt(delta)) / 2 * a;
+
+                message = "Les solutions de l'équations sont: " + x1 + " et " + x2;
+            }
+            else if(delta == 0)
+            {
+                double x = -b / 2 * a;
+
+                message = "La solution est : " + x;
+            }
+            else
+            {
+                message = "Il n'y a pas de solutions à cette équation";
+            }
+        }
+    }
+}
