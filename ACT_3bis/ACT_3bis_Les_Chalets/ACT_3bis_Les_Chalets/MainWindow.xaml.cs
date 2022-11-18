4163bc7b39d3c3663b6bbdaa3ac29dc5ffb76a69
Tarnus Gabriel <200198@CCM-Asty-Moulin.local>
2022-11-18 14:05:18 +0100

    Les chalets

---
+++ ACT_3bis/ACT_3bis_Les_Chalets/ACT_3bis_Les_Chalets/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ACT_3bis_Les_Chalets
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            nbrPersInput.PreviewTextInput += new TextCompositionEventHandler(VerifNombrePersonne);
+        }
+
+        public void VerifNombrePersonne(object sender, TextCompositionEventArgs e)
+        {
+            int nbrPersonnes;
+            if(int.TryParse(e.Text, out nbrPersonnes))
+            {
+                if(nbrPersonnes < 1 && nbrPersonnes > 6)
+                {
+                    e.Handled = true;
+                }
+                else
+                {
+                    messageEntier.Text = "Vous devez mettre un nombre entre 1 et 6!!";
+                }
+            }
+            else
+            {
+                messageEntier.Text = "Vous devez mettre un nombre entre 1 et 6!!";
+            }
+        }
+    }
+}
