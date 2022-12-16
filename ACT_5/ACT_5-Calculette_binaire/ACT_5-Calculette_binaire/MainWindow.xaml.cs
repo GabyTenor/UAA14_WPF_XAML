dbfa26118bee10e9a333208aa9a231cbb3afc9e9
Tarnus Gabriel <200198@CCM-Asty-Moulin.local>
2022-12-16 14:24:42 +0100

    Calculette procédurale

--- ACT_5/ACT_5-Calculette_binaire/ACT_5-Calculette_binaire/MainWindow.xaml.cs
+++ ACT_5/ACT_5-Calculette_binaire/ACT_5-Calculette_binaire/MainWindow.xaml.cs
@@ -77,14 +77,133 @@ namespace ACT_5_Calculette_binaire
 
         public void Calculer(object sender, RoutedEventArgs e)
         {
-            if(nbrB1.Text != "" && nbrB2.Text != "" && (add.IsChecked = true || sous.IsChecked = true))
+            if(nbrB1.Text != "" && nbrB2.Text != "" && (add.IsChecked == true || sous.IsChecked == true))
             {
+                result.Text = "";
+                ushort[] nbr1 = RemplirTableau(nbrB1.Text);
+                ushort[] nbr2 = RemplirTableau(nbrB1.Text);
+                bool ok;
+                ushort[] tRes;
+                
+                if (add.IsChecked == true)
+                {
+                    Additionner(nbr1, nbr2, out tRes, out ok);
 
+                    if (!ok)
+                    {
+                        result.Text = "Le résultat dépasse les 7 bits";
+                    }
+                    else
+                    {
+                        for (int i = 0; i < tRes.Length; i++)
+                        {
+                            result.Text += tRes[i];
+                        }
+                    }
+                }
+                if(sous.IsChecked == true)
+                {
+                    if(!Soustraction(nbr1, nbr2, out tRes))
+                    {
+                        result.Text = "Le résultat dépasse les 7 bits";
+                    }
+                    else
+                    {
+                        for (int i = 0; i < tRes.Length; i++)
+                        {
+                            result.Text += tRes[i];
+                        }
+                    }
+                }
+                
             }
             else
             {
                 result.Text = "Veuillez remplir tous les chants de texte et sélectionner une option";
             }
+        }
+
+        public void Additionner(ushort[] t1, ushort[] t2, out ushort[] tRes, out bool ok)
+        {
+            ok = true;
+            ushort report = 0;
+            ushort res;
+
+            tRes = new ushort[8];
+
+            for (int i = 7; i > -1; i--)
+            {
+                res = (ushort)(t1[i] + t2[i] + report);
+
+                if(res/2 == 0)
+                {
+                    report = 0;
+                }
+                else
+                {
+                    report = 1;
+                }
+
+                if(res == 1)
+                {
+                    tRes[i] = 1;
+                }
+                else
+                {
+                    if(res % 2 == 1)
+                    {
+                        tRes[i] = 1;
+                    }
+                    else
+                    {
+                        tRes[i] = 0;
+                    }
+                }
+                if(report == 1)
+                {
+                    ok = false;
+                }
+            }
+        }
+
+        public bool Soustraction(ushort[] t1, ushort[] t2, out ushort[] tRes)
+        {
+            int[] tTemp = new int[8];
+            tRes = new ushort[8];
+
+            bool ok = true;
+
+            for (int i = 0; i < 8; i++)
+            {
+                tTemp[i] = t1[i] - t2[i];
+            }
+
+            for(int j = 0; j > -1; j--)
+            {
+                if(tTemp[j] == -1)
+                {
+                    t2[j - 1] = (ushort)(t2[j - 1] + 1);
+                    t1[j] = (ushort)(t1[j] + 2);
+                }
+
+                if(t1[j] < t2[j])
+                {
+                    t2[j - 1] = (ushort)(t2[j - 1] + 1);
+                    t1[j] = (ushort)(t1[j] + 2);
+                    tRes[j] = (ushort)(t1[j] - t2[j]);
+                }
+            }
+
+            if(t1[0] >= t2[0])
+            {
+                tRes[0] = (ushort)(t1[0] - t2[0]);
+            }
+            else
+            {
+                ok = false;
+            }
+
+            return ok;
         }
     }
 }
---
+++ ACT_5/ACT_5-Calculette_binaire/ACT_5-Calculette_binaire/NombreBinaire.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ACT_5_Calculette_binaire
+{
+    class NombreBinaire
+    {
+
+    }
+}
